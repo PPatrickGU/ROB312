868115a95483cd3cb09870b6f0e240bafbdfc602
dajing <gudajing@yeah.net>
2020-12-23 04:17:02 +0100

    update

---
+++ TP3 Filtrage Particulaire/stratified.m
@@ -0,0 +1,16 @@
+function [index] = stratified(wp)
+    N = length(wp);
+    index = zeros(1,N);
+    Q = cumsum(wp); 
+    n = 1;
+    m = 1;
+    while (n <= N && m <= N)
+        u0 = randn/N;
+        u = u0 + n/N;
+        while (Q(m) < u && m < N)
+            m = m+1;
+        end
+        index(n) = m;
+        n = n+1;
+    end
+end
